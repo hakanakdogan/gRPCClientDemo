8c6aafa42a8d90472118330c43323d42d987cda6
Hakan Akdoğan <hakan.akdogan@ozdilek.com.tr>
2023-07-03 16:36:30 +0300

    Add project files.

---
+++ gRPCClientDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using gRPCClientDemo;
+
+
+var data = new OrderRequest { OrderId = 1 };
+var grpcChannel = GrpcChannel.ForAddress("https://localhost:7016");
+var client = new OrderProcessing.OrderProcessingClient(grpcChannel);
+var response = await client.GetOrderAsync(data);
+Console.WriteLine("Order Quantity: {0}", response.Order.OrderQuantity);
+Console.ReadLine();
